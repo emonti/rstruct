51a2eff1dd7be5b9df9ea184ed43123c39f48f79
Eric Monti <esmonti@gmail.com>
2011-04-26 18:07:32 -0500

    stub spec

---
+++ spec/structure_spec.rb
@@ -0,0 +1,8 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Rstruct::Structure do
+  it "should be a class" do
+    Rstruct::Structure.should be_a(Class)
+  end
+end
+
