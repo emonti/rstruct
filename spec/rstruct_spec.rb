460abc9b3431a1f1103c7ee6d86ce8241be09a70
Eric Monti <esmonti@gmail.com>
2011-04-27 16:41:14 -0500

    several more unit tests for struct method

--- spec/rstruct_spec.rb
+++ spec/rstruct_spec.rb
@@ -56,19 +56,66 @@ describe Rstruct do
       s.fields.each{|f| f.should be_kind_of(Rstruct::Int32) }
     end
 
+    it "should allow a structure to register itself in a registry and still access default types" do
+      # create a registry for this test
+      reg = Rstruct::Registry.new(:rstruct_struct_test_reg1)
+
+      # create a struct which registers itself to this registry
+      s = Rstruct.struct(:rspec_test_struct1, :register => reg) {
+        int32   :someint1
+        int32   :someint2
+      }
+
+      # confirm declaration went ok
+      s.fields.should be_an(Array)
+      s.field_names.should == [:someint1, :someint2]
+      s.fields.each{|f| f.should be_kind_of(Rstruct::Int32) }
+
+      # confirm the struct is registered
+      reg[:rspec_test_struct1].should == s
+    end
+
+    it "should allow structure fields to come from the registry the struct is registered to" do
+      # create a registry for this test
+      reg = $rstruct_struct_reg_test_reg = Rstruct::Registry.new(:rstruct_struct_test_reg2)
+
+      # create a test type registered to this registry
+      c = Class.new(Rstruct::Type)
+      c.register :reg_test_typ, $rstruct_struct_reg_test_reg
+      reg[:reg_test_typ].should == c
+
+      # create a struct which registers itself to this registry
+      # and declares a field of the above type
+      s = Rstruct.struct(:rspec_test_struct2, :register => reg) {
+        int32   :someint1
+        int32   :someint2
+        reg_test_typ :sometype
+      }
+
+      # confirm the struct is registered
+      reg[:rspec_test_struct2].should == s
+
+      # confirm declaration went ok
+      s.fields.should be_an(Array)
+      s.field_names.should == [:someint1, :someint2, :sometype]
+      s.fields[0,2].each{|f| f.should be_kind_of(Rstruct::Int32) }
+      s.fields.last.should be_kind_of(c)
+
+    end
+
     it "should not register a structure by default" do
       (s=Rstruct.struct(:rspec_not_reg_dflt){}).should be_a(Rstruct::Structure)
       Rstruct.default_registry.get(:rspec_not_reg_dflt).should be_nil
     end
 
+    it "should allow a struct to explicitely opt out of registration" do
+      (s=Rstruct.struct(:rspec_not_reg, :register=>false){}).should be_a(Rstruct::Structure)
+      Rstruct.default_registry.get(:rspec_not_reg).should be_nil
+    end
+
     it "should allow a struct to register itself with the default registry" do
       (s=Rstruct.struct(:rspec_registered, :register=>true){}).should be_a(Rstruct::Structure)
       Rstruct.default_registry.get(:rspec_registered).should == s
-    end
-
-    it "should allow a struct to explicitely opt out of registration" do
-      (s=Rstruct.struct(:rspec_not_reg, :register => false){}).should be_a(Rstruct::Structure)
-      Rstruct.default_registry.get(:rspec_not_reg).should be_nil
     end
 
   end
